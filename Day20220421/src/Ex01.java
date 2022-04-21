0e51d5483c5905bc5662b1023c33b0586c8188d2
sujung <sjlee954@naver.com>
2022-04-21 12:16:01 +0900

    modi

--- Day20220421/src/Ex01.java
+++ Day20220421/src/Ex01.java
@@ -1,8 +1,45 @@
+import java.util.Scanner;
 
 public class Ex01 {
 
 	public static void main(String[] args) {
-		System.out.println("깃허브");
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int num3 = sc.nextInt();
+		
+//		int max = num1;
+//		
+//		if (num2 > max) 
+//			max = num2;
+//		
+//		if (num3 > max) 
+//			max = num3;
+//		
+//		System.out.println(max);
+		
+		System.out.println("------------------------------");
+		int max = num1;
+		int tmp=0;
+		
+		if (num2 > max) { 
+			tmp = max;
+			max = num2;
+			num2 = tmp;
+			if (num3 > max) {
+				tmp = max;
+				max = num3;
+				num3 = tmp;
+			}
+		}
+		if (num3 > num2) {
+			tmp = num3;
+			num3 = num2;
+			num2 = tmp;
+		}
+		
+		System.out.println(max+","+num2+","+num3);
+		sc.close();
 	}
 
 }
